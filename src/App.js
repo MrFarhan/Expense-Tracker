7091b1e4149b9c035d57ae3af56a6d4c40791b3d
MrFarhan <muhammadfarhan02345@gmail.com>
2020-07-20 22:17:22 +0500

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Header,Balance, IncomeExpense,TransactionHistory,AddTransaction} from './components/main'
-
+import { GlobalProvider } from './context/GlobalContext'
 
 
 function App() {
   return (
+    <GlobalProvider>
     <div className="container">
       <Header />
       <Balance/>
@@ -13,6 +14,7 @@ function App() {
       <TransactionHistory/>
       <AddTransaction/>
     </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Input, Button, Label, Form } from 'reactstrap';
+import { GlobalContext } from '../context/GlobalContext'
 
 
 export const Header = () => {
@@ -7,57 +8,96 @@ export const Header = () => {
         <div><h2>
             Expense Tracker
         </h2>
-        <br/></div>
+            <br /></div>
     )
 }
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <div className="center">
-            <h3>
+            <h4>
                 Current Balance
-            </h3>
-            <h3>
-                $ 500</h3 >
+            </h4>
+            <h1>
+                $ {total}</h1 >
         </div>
     )
 }
 
 export const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc, item), 0) * -1
+    ).toFixed(2);
+
     return (
         <div className="incomeExpense">
-            <h3 className="income">Income<span><br />$ 200 </span></h3>
+            <h3 className="income">Income<span><br />$ {income} </span></h3>
 
 
-            <h3 className="expense">Expense<span><br />$ 320 </span></h3>
+            <h3 className="expense">Expense<span><br />$ {expense} </span></h3>
         </div>
     )
 }
 
 export const TransactionHistory = () => {
+    const { transactions } = useContext(GlobalContext);
+    // console.log(context, "is context");
+
     return (
-        <h2>
-            Transaction History
-            <hr />
-        </h2>
+        <div>
+            <h3>Transaction History</h3>
+            <ul>
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />
+                ))}
+            </ul>
+
+        </div>
     )
 }
 
+
 export const AddTransaction = () => {
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const {addTransaction} = useContext(GlobalContext)
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount : +amount
+        }
+        addTransaction(newTransaction);
+    }
     return (
         <div>
-            <h2>
+            <h4>
                 Add Transaction
-            </h2>
+            </h4>
             <hr />
-            <Form>
-                <h3> Add Discription</h3>
+            <Form onSubmit={onSubmit}>
+                <h4> Add Discription</h4>
                 <Label htmlFor="description">
-                    <Input type="text" id="description" placeholder="Add Discription" /></Label>
+                    <Input type="text" id="description" placeholder="Add Discription" value={text} onChange={(e) => setText(e.target.value)} /></Label>
 
-                <h3> Add Amount</h3>
+                <h4> Add Amount</h4>
                 <Label htmlFor="amount">
-                    <Input id="amount" type="text" placeholder="Add Amount" ></Input></Label>
+                    <Input id="amount" type="text" placeholder="Add Amount" value={amount} onChange={(e) => setAmount(e.target.value)} ></Input></Label>
                 <br />
                 <br />
                 <Button color="primary" >Add Transaction</Button>
@@ -69,3 +109,13 @@ export const AddTransaction = () => {
     )
 }
 
+export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    const sign = transaction.amount > 0 ? "+" : "-"
+    return (
+        <div>
+            <li className={transaction.amount > 0 ? "plus" : "minus"}>{transaction.text} <span>{sign}${Math.abs(transaction.amount)}   </span> <Button onClick={() => deleteTransaction(transaction.id)} className="delete-btn"> X</Button></li>
+        </div>
+    )
+}
